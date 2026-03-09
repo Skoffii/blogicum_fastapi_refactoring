73c413adbd90125b641c5bbb00cfcdaaefad3cd6
Skofi <kashirskih.sofia@yandex.ru>
2026-03-09 16:19:32 +0400

    test db

---
+++ fastapi_app/src/test_db.py
@@ -0,0 +1,14 @@
+posts_db: list[dict] = []
+post_id = 1
+
+categories_db: list[dict] = []
+category_id = 1
+
+comments_db: list[dict] = []
+comment_id = 1
+
+location_db: list[dict] = []
+location_id = 1
+
+users_db: list[dict] = []
+user_id = 1
